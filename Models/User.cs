ce022af5f8e46ea9191c4bf3f3593bb8f0b6b34f
SandraRadz <oleksandraradzievska@gmail.com>
2019-02-22 15:39:09 +0200

    start

---
+++ Models/User.cs
@@ -0,0 +1,34 @@
+﻿namespace Radzievska_Homework1
+{
+    class User
+    {
+        private string _birthday;
+        private int _age;
+        private string _zodiac;
+        private string _chinaZodiac;
+
+        public string Birthday
+        {
+            get { return _birthday; }
+            set {_birthday = value;}
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        public string Zodaic
+        {
+            get { return _zodiac; }
+            set { _zodiac = value; }
+        }
+        public string ChinaZodiac
+        {
+            get { return _chinaZodiac; }
+            set { _chinaZodiac = value; }
+        }
+
+    }
+}
